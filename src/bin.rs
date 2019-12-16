8b294656916f9e9010157d05571d1e0ec6ee9561
Finnkauski <iwiivi@gmail.com>
2019-12-16 20:44:07 +0000

    fix cargo manifest

--- src/bin.rs
+++ src/bin.rs
@@ -7,20 +7,14 @@ use serde_json::json;
 
 fn main() {
     dotenv::dotenv().ok();
-    // let on = match env::var("SW").unwrap().as_ref() {
-    //     "ON" => true,
-    //     "OFF" => false,
-    //     _ => false,
-    // };
 
     let ip = env::var("IP").unwrap();
     let key = env::var("KEY").unwrap();
+    let bri: u16 = env::var("BRI").unwrap().parse().unwrap();
 
     let bridge = Bridge::link(ip, key);
 
     // TODO: make name detection automatic
     // TODO: add usage of structs for state
-    bridge.state_mult(vec![1, 2, 3], &json!({"on":false, "transitiontime":1}));
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    bridge.state_mult(vec![1, 2, 3], &json!({"on":true, "transitiontime":1}));
+    bridge.state_mult(vec![1, 2, 3], &json!({"bri":bri,"transitiontime":1}));
 }
